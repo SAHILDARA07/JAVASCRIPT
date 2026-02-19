1f9366c7b19691eacc88ad3663f4d36c11a11c7f
SAHIL <sahildara152@gmail.com>
2026-02-19 18:02:19 +0530

    Add files via upload

---
+++ DAY-1/script.js
@@ -0,0 +1,27 @@
+// javascript - javascript is programming language 
+// which is used make web  page interactive.
+
+//website - is the collection of multiple webpages.
+//web page - a single document which contain some information/data.
+
+
+//html - layout
+//css - style
+//java - interactive
+
+//programing
+//program -> set of instruction/command which is used to perform
+//specific task from the computer.
+// process of writing program(code) is a programing.
+
+
+//computer -> powerfull machine
+// it cant do anything without command.
+
+// html+css=js = website   
+
+// javascript can run only withing html.
+
+// Two ways to connect javascript in html
+//internal - script tag
+//external - script tag
