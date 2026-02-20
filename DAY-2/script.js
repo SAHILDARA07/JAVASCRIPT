bcd59d3e8273974c2d8c9e363b4a18b113698a76
SAHIL <sahildara152@gmail.com>
2026-02-20 17:57:47 +0530

    Add files via upload

---
+++ DAY-2/script.js
@@ -0,0 +1,44 @@
+// javascript run only in browser threw html.
+// .js -> extension used to create js file.
+// what is importance of extension ?
+// by extension is identity of file which help to known content/information inside the file.
+
+// console.log() ->
+// document.writeln() ->
+
+// datatype ->
+// data ? -> information
+// number -> 0 to 9, 12.34, 12,-90,0,1234567777777777, 0 to 9, 12.34, 12,-90,0,
+// string(text) -> ALL  characters and alphanumeric values  are consider in string
+//ex. kunal,k,k9,"123",'123'
+
+// object
+    // Array - collection of multiple values
+    // object - collection of multiple key-value pair.
+
+// implict(js) & explict(c)
+
+// website -> user  input -> process -> output
+
+// variables - is one type of container where we can store a value.
+
+// form
+// email ->
+// password ->
+// button
+
+
+// variable(truck)
+
+// to define variable in javascript there are 3 keywords
+// var,let,const
+
+
+let a = 10;
+let b = 30;
+let p = "1";
+let q = "1";
+console .log(a)
+console.log(b)
+console.log(a + b)
+console.log(p + q)
