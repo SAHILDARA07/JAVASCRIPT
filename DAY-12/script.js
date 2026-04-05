14c412edadf667c9ec331c8298ccc09de2804ff4
SAAHIL <sahildara152@gmail.com>
2026-04-05 10:51:40 +0530

    Add files via upload

---
+++ DAY-12/script.js
@@ -0,0 +1,29 @@
+// variable - is one type of container when we can store value and also we can change  the value.
+
+// data structure - how to store data way to store data.
+
+// Array - is one type of data structure when we store multiple value.
+
+// [] <- symbol of array
+
+let a = [20,20,30,3,4,6,7]
+
+console.log(a)  // to print whole array.
+
+console.log(a[1]) // to prnt 2nd value
+
+let b = [23,43,55,64,7,4,4]
+
+for (let i = 0; i < a.length; i++){ //  by using loop 
+    console.log(a[i])
+}
+
+
+let arr = ["apple","banana","mango","kiwi"]
+
+let i = 0;
+while (i < arr.length) {
+    console.log(object)
+    i++;
+}
+
