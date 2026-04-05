9da72792e23d144b6b606c024f40cd2f73d75b63
SAAHIL <sahildara152@gmail.com>
2026-04-05 11:28:08 +0530

    Add files via upload

---
+++ DAY-13/script.js
@@ -0,0 +1,19 @@
+let arr =[1,2,3,4,5,6 ]
+
+
+arr.push(); //insert value in array at last position.
+arr.pop(); // to remove last value of the array.
+
+arr.unshift(17); // to insert value at the starting in array.
+arr.shift(); // it remove first element in the array.
+
+const v1 = arr.at(2) // return the element located at the specified index.
+
+const v2 = arr[2]
+
+arr.slice(1,3); // 
+arr.splice(); // index,count(how many values want to remove from index)
+arr.forEach(()=>{      // to perform the specified action for each element in array.
+    console.assert.log(e)
+});
+//console.log(arr)
