bf952f255fa5f17f53965479f4d547430b27a646
SAAHIL <sahildara152@gmail.com>
2026-04-05 11:33:11 +0530

    Add files via upload

---
+++ DAY-3/script.js
@@ -0,0 +1,20 @@
+// wap to swap(interchange) values of two variables without using third variable.
+let a = 20; // a = 20
+let b = 30; // b = 30
+
+a = a + b; // a = 20 + 30 = 50
+b = a - b; // b = 50 - 30 = 20
+a = a - b; // a = 50 - 20 = 30
+   
+console.log("a =",a);
+console.log("b =",b);
+
+// datatype
+// variable
+// operators - 4 types
+
+// operators - opeator is one type of symbol which is used  to perform operation on the value.
+
+// 1.. unary operator - unary operator need one operand to perform operation.
+// 
+// 2.. binary operator - binary operator need minimum two operands to perform.
