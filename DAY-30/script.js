a1655429c3a211f0b3d488466e33326dc531abc6
SAAHIL <sahildara152@gmail.com>
2026-04-23 22:40:13 +0530

    Add files via upload

---
+++ DAY-30/script.js
@@ -0,0 +1,39 @@
+const productsdivison = document.getElementById("products-div")
+
+function addtocart(product){
+    console.log(product)
+}
+
+function displayProducts(products){
+    products.map((product,i) => {
+        const div = document.createElement("div"); //   <div></div>
+        div.className = "card p-0"
+        div.style.width = "18rem";
+       div.innerHTML = ` 
+                <img src=${product.images[0]}
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${product.title}</h5>
+                    <p class="card-text">${product.category}.</p>
+                    <p class="card-text fs-5">price  ₹ ${(product.price * 93).toFixed(2)}/-.</p>
+                    <button class="btn btn-primary">Add to Cart</button>
+                </div>
+            </div>`;
+
+        productsdivison.appendChild(div);
+    })
+
+
+}  
+
+
+
+async function fetchproduct() {
+    const res = await fetch("https://dummyjson.com/products")
+    const data = await res.json();
+    displayProducts(data.products)
+}
+
+
+
+fetchproduct();
